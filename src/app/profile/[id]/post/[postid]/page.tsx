cbe3180573d1b66f1971d3b0d852e2bd456a8743
Alex Volkov <alexandr.volkoff@gmail.com>
2023-11-14 13:52:52 +0000

    Router

--- src/app/profile/[id]/post/[postid]/page.tsx
+++ src/app/profile/[id]/post/[postid]/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+import { useParams, useRouter } from "next/navigation";
+
 export default function ProfileIDPostsID() {
-    return <div>Profile ID Posts page</div>;
+    const params = useParams();
+    const router = useRouter();
+
+    
+    return (
+        <div>
+            <h1>Profile page ID: {params.id}</h1>
+            <div onClick={() => router.push("/profile")}>Back to main page</div>
+        </div>
+    );
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,19 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function Profile() {
-    return <div>Profile Page</div>;
+    const router = useRouter();
+    console.log(router);
+
+    return (
+        <div>
+            <h1>List of profiles</h1>
+            <ul>
+                <li onClick={() => router.push("/profile/1")}>Profile 1</li>
+                <li onClick={() => router.push("/profile/2")}>Profile 2</li>
+                <li onClick={() => router.push("/profile/3")}>Profile 3</li>
+            </ul>
+        </div>
+    );  
 }
