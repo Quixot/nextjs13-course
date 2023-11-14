791b00bb431d1240fb9ba60dd356ac7bb62afb69
Alex Volkov <alexandr.volkoff@gmail.com>
2023-11-14 16:45:22 +0000

    Link

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function Profile() {
@@ -8,11 +9,29 @@ export default function Profile() {
 
     return (
         <div>
-            <h1>List of profiles</h1>
+            <h1>List of profiles using router</h1>
             <ul>
                 <li onClick={() => router.push("/profile/1")}>Profile 1</li>
                 <li onClick={() => router.push("/profile/2")}>Profile 2</li>
                 <li onClick={() => router.push("/profile/3")}>Profile 3</li>
+            </ul>
+
+            <h1>List of profiles using Link</h1>
+            <ul>
+                <Link href='/profile/1'>Profile 1</Link>
+                <Link href='/profile/2'>Profile 2</Link>
+                <Link 
+                    scroll={false}
+                    prefetch={true}
+                    href={{
+                        pathname: "/profile/3",
+                        query: {
+                            userName: "Alex",
+                        },
+                    }}
+                >
+                    Profile 3
+                </Link>
             </ul>
         </div>
     );  
