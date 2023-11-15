bf97224fa6c459a6c7c5c86b8ff446ed1aced089
Alex Volkov <alexandr.volkoff@gmail.com>
2023-11-15 15:00:02 +0000

    Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+"use client";
+
 import Image from 'next/image'
+import MyHeader from '@/components/Header';
+import logotype from '../../public/next.svg';
 
 export default function Home() {
+  const name = "Alex"
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hellow, This is my Main Page!
+      <MyHeader name={name} age={47} />
+      <h1>Hellow, This is my Main Page!</h1>
+      <Image src='/next.svg' width={400} height={200} alt='Logo' />
+      <div className="w-[500px]">
+        <Image quality={75} src={logotype} alt="Logo2" />
+        {/* https://nextjs.org/docs/app/api-reference/components/image */}
+      </div>
+      
     </main>
   )
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+"use client";
+import Logo from './Logo';
+import Menu from './Menu';
+
+export default function Header(props: any) {
+    return (
+        <header>
+            <Logo />
+            <Menu {...props} />
+        </header>
+    );
+}
